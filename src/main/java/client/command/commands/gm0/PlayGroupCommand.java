c14bde17b09c47730e925b06b6b77b0b078e4440
Arthur Charlton <LynxStar@kervana.com>
2023-07-02 13:21:24 -0400

    More info on distance

--- src/main/java/client/command/commands/gm0/PlayGroupCommand.java
+++ src/main/java/client/command/commands/gm0/PlayGroupCommand.java
@@ -24,10 +24,11 @@ public class PlayGroupCommand extends Command {
         var level = character.getLevel();
 
         var median = (int)Math.round(levelData.getThird());
+        var average = levelData.getSecond() - levelData.getFirst();
 
         var diff = level - median;
         character.yellowMessage(String.format("[Playgroup]: %d to %d Median: [%d]", levelData.getFirst(), levelData.getSecond(), median));
-        character.yellowMessage(String.format("[Distance]: %d", diff));
+        character.yellowMessage(String.format("[Distance]: %d from Centerpoint: %d", diff, average));
         character.yellowMessage(String.format("[EXP Rate]: %.2f%%", character.playgroupEXPRate * 100));
         character.yellowMessage(String.format("[Drop Rate]: %.2f%%", character.playgroupDropRate * 100));
 
